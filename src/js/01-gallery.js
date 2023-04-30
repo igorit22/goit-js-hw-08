b53cf484fba1fe9007cb20858573a884a5a7d7c7
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-30 17:29:09 +0300

    Update 01-gallery and 02-video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,9 @@
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-// Імпортуємо масив елементів галереї з іншого файлу
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 
-// Знаходимо контейнер галереї на сторінці
 const galleryContainer = document.querySelector('.gallery');
 
-// Функція, яка створює HTML-розмітку для елемента галереї
 const createGalleryItem = ({ preview, original, description }) =>
   `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
@@ -22,57 +16,11 @@ const createGalleryItem = ({ preview, original, description }) =>
     </a>
   </li>`;
 
-// Створюємо HTML-розмітку для всіх елементів галереї
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 
-// Додаємо HTML-розмітку до контейнера галереї
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// Додаємо обробник події на контейнер галереї
-galleryContainer.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  // Перевіряємо, чи був натиснутий елемент з тегом 'img'
-  const isGalleryImageEl = event.target.nodeName === 'IMG';
-
-  if (!isGalleryImageEl) {
-    return;
-  }
-
-  // Отримуємо посилання на зображення та його опис
-  const galleryImageSrc = event.target.dataset.source;
-  const galleryImageAlt = event.target.alt;
-
-  // Створюємо екземпляр модального вікна та показуємо його
-  const instance = basicLightbox.create(
-    `
-      <img src="${galleryImageSrc}" alt="${galleryImageAlt}">
-    `,
-    {
-      onShow: (instance) => {
-        console.log('onShow', instance);
-        window.addEventListener('keydown', (event) => onEscKeyPress(event, instance));
-      },
-      onClose: (instance) => {
-        console.log('onClose', instance);
-        window.removeEventListener('keydown', (event) => onEscKeyPress(event, instance));
-      },
-    }
-  );
-
-  instance.show();
-
-  // Додаємо слухачів подій
-  window.addEventListener('keydown', (event) => onEscKeyPress(event, instance));
-  instance.element().addEventListener('click', () => {
-    instance.close();
-    window.removeEventListener('keydown', (event) => onEscKeyPress(event, instance));
-  });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-
-// Додаємо обробник події на клавішу 'Escape' для закриття модального вікна
-function onEscKeyPress(event, instance) {
-  if (event.code === 'Escape') {
-    instance.close();
-  }
-}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const currentTimeKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function(event) {
+  localStorage.setItem(currentTimeKey, event.seconds.toString());
+}, 1000));
+
+const savedTime = localStorage.getItem(currentTimeKey);
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('play', function() {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
